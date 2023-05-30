385ee9ed3663f875e210684ebebae2a9d6479d74
Arghadeep Dey <deyarghadeep23@gmail.com>
2023-05-30 19:08:50 +0530

    custom favicon and title - Video 15

--- src/App.js
+++ src/App.js
@@ -38,11 +38,19 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
       showAlert("Dark mode has been enabled", "success");
+      document.title='TextUtils - DarkMode'
+      // setInterval(()=>{
+      //   document.title='Install TextUtils now'
+      // },2000);
+      // setInterval(()=>{
+      //   document.title='TextUtils is amazing!'
+      // },1500);
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
+      document.title='TextUtils - LightMode'
     }
   }
   return (
